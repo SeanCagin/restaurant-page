ee997c794e817f66efea314a7b5764df993c5fdf
Sean Uz <cuz3@gatech.edu>
2024-02-02 01:59:03 -0500

    Fix typo in aboutpage.js

--- src/aboutpage.js
+++ src/aboutpage.js
@@ -6,7 +6,7 @@ function generatePage() {
 
     title.textContent = 'This is the Casino Restaurant Menu!';
     description.textContent = `Gambling addicts from all around the world have gathered
-                                to create this casino webiste (jk don't sue me). For security purposes
+                                to create this casino website (jk don't sue me). For security purposes
                                 we do not include the contact information here. Try to see if you can figure
                                 out the puzzle and reach the next step (or maybe there is no puzzle...)`;
 
